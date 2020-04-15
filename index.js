5b38e3647706849360f6b3b8bb227fe16105de92
Tawhid Ali <tawhidali126@gmail.com>
2020-04-15 02:13:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Example 1:
+// Input: 121
+// Output: true
+
+
+// Example 2:
+// Input: -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+
+// Example 3:
+// Input: 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+
+var isPalindrome = function(x) {
+    xString = x.toString().split('').reverse().join('')
+    return (x.toString() === xString)
+};
